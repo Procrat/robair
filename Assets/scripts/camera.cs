38f68c0617c48288a3db91a19754b2e6751d350a
Stijn Seghers <stijnseghers@gmail.com>
2020-02-01 19:02:11 +1300

    Big brother is watching you

---
+++ Assets/scripts/camera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public GameObject player1;
+    public GameObject player2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var lerp = Vector2.Lerp(player1.transform.position, player2.transform.position, 0.5f);
+        transform.position = new Vector3(lerp.x, lerp.y, transform.position.z);
+    }
+}
